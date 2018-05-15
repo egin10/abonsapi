3a80f0a5a785e001ba5eb96420c5dc47ea4feec9
Ginanjar S.B <egin066@gmail.com>
2018-05-15 21:00:18 +0700

    Update app.js

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ bot.on("message", async message => {
     if(message.channel.type === "dm") return;
 
     let prefix = botconfig.prefix;
+    if (!message.content.startsWith(prefix)) return;
+    
     let messageArray    = message.content.split(" ");
     let cmd             = messageArray[0];
     let args            = messageArray.slice(1);
