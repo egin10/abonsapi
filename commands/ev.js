66569a6b2f454aa6e19c66cdf54677a03ec3f667
Ginanjar <egin066@gmail.com>
2018-05-16 18:30:35 +0700

    edit ev

--- commands/ev.js
+++ commands/ev.js
@@ -10,16 +10,36 @@ module.exports.run = async (bot, message, args) => {
      */
 
     if(message.author.id !== '378940242876432396') return;
-    let code = args.join(" ");
+    const code = args.join(" ");
     
-    let eval = new Discord.RichEmbed()
-    .setTitle("**Evaluate JavaScript**")
-    .setColor("RANDOM")
-    .addField(":inbox_tray: **Input**", code)
-    .addField(":outbox_tray: **Output**", eval(toString(code)));
+    // let eval = new Discord.RichEmbed()
+    // .setTitle("**Evaluate JavaScript**")
+    // .setColor("RANDOM")
+    // .addField(":inbox_tray: **Input**", code)
+    // .addField(":outbox_tray: **Output**", eval(toString(code)));
     
-    message.channel.send(eval);
-    
+    // message.channel.send(eval);
+    try {
+        const evaled = eval(code);
+        const clean = await bot.clean(bot, evaled);
+        if (code) {
+            const embed = new Discord.RichEmbed()
+            .setAuthor(`${message.author.tag}`, message.author.avatarURL)
+            .setColor("RANDOM")
+            .addField(":inbox_tray: **Input**", `${code}`)
+            .addField(":outbox_tray: **Output**", `\`\`\`js\n${clean}\n\`\`\``)
+            message.channel.send(embed);
+        } else {
+            const embed = new Discord.RichEmbed()
+            .setAuthor(`${message.author.tag}`, message.author.avatarURL)
+            .setColor("RANDOM")
+            .addField(":inbox_tray: **Input**", `none`)
+            .addField(":outbox_tray: **Output**", `\`\`\`js\n${clean}\n\`\`\``)
+            message.channel.send(embed);
+        }
+    } catch (err) {
+        message.channel.send(`\`ERROR\` \`\`\`xl\n${await bot.clean(bot, err)}\n\`\`\``);
+    }
 }
 
 module.exports.help = {
